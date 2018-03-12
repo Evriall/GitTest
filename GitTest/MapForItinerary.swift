f254d78a83c11bb269f284119c209d59bdcb981d
Sergey Guznin <sergeyguznin@sergeys-macbook-pro.local>
2018-03-12 16:33:47 +0200

    Add new map for itinerary feature.

---
+++ GitTest/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitTest
+//
+//  Created by Sergey Guznin on 3/12/18.
+//  Copyright © 2018 Sergey Guznin. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
